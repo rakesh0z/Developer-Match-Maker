ad50047846779b76f826c5660ca24f47975a5478
Rakesh <rmahapatra471@gmail.com>
2026-06-12 23:05:50 +0530

    2s

---
+++ server/src/services/github.service.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export const getUserRepos = async (
+  accessToken: string
+) => {
+
+  const response = await axios.get(
+    "https://api.github.com/user/repos",
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      },
+      params: {
+        per_page: 100
+      }
+    }
+  );
+
+  return response.data;
+};
